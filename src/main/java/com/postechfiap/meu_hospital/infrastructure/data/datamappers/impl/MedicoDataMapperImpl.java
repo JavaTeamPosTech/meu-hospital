af797495a24cd8eaf9ec59b7a0a75f449ff78953
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2025-09-28 23:49:18 -0300

    enviando dados para os construtores de entidade medico

--- src/main/java/com/postechfiap/meu_hospital/infrastructure/data/datamappers/impl/MedicoDataMapperImpl.java
+++ src/main/java/com/postechfiap/meu_hospital/infrastructure/data/datamappers/impl/MedicoDataMapperImpl.java
@@ -2,6 +2,7 @@ package com.postechfiap.meu_hospital.infrastructure.data.datamappers.impl;
 
 import com.postechfiap.meu_hospital.core.domain.entities.MedicoDomain;
 import com.postechfiap.meu_hospital.infrastructure.data.datamappers.MedicoDataMapper;
+import com.postechfiap.meu_hospital.infrastructure.model.EnderecoEntity;
 import com.postechfiap.meu_hospital.infrastructure.model.MedicoEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -16,7 +17,27 @@ public class MedicoDataMapperImpl implements MedicoDataMapper {
             return null;
         }
 
-        return null;
+        EnderecoEntity enderecoEntity = new EnderecoEntity(
+                medicoDomain.getEndereco().getEstado(),
+                medicoDomain.getEndereco().getCidade(),
+                medicoDomain.getEndereco().getBairro(),
+                medicoDomain.getEndereco().getRua(),
+                medicoDomain.getEndereco().getNumero(),
+                medicoDomain.getEndereco().getComplemento(),
+                medicoDomain.getEndereco().getCep()
+        );
+
+        return new MedicoEntity(
+                medicoDomain.getNome(),
+                medicoDomain.getEmail(),
+                medicoDomain.getLogin(),
+                medicoDomain.getSenha(),
+                medicoDomain.getDataNascimento(),
+                medicoDomain.getCpf(),
+                medicoDomain.getCrm(),
+                medicoDomain.getSalario(),
+                enderecoEntity
+        );
     }
 
     @Override
